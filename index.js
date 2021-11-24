533e62f4caa88609fc9815c565897efb623496cf
Gabriel <gaba93@yandex.ru>
2021-11-24 10:54:07 +0300

    connection to MongoDB, data Schema, post req endpoint

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Post from './Post.js';
 
 const PORT = 5000;
+const DB_URL =
+  'mongodb+srv://user:user@cluster0.4ie6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const app = express();
 
-app.post('/', (request, response) => {
-  console.log(request.body);
-  response.status(200).json('Server is working');
+app.use(express.json());
+
+app.post('/', async (request, response) => {
+  try {
+    const { author, title, content, picture } = request.body;
+    const post = await Post.create({ author, title, content, picture });
+    response.status(200).json(post);
+  } catch (e) {
+    response.status(500).json(e);
+  }
 });
 
-app.listen(PORT, () => console.log(`THE SERVER STARTED ON PORT ${PORT}`));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => console.log(`THE SERVER STARTED ON PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
