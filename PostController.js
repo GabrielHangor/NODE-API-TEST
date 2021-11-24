7802fbf654c581429ed31d1cc60662e53c7a1af0
Gabriel <gaba93@yandex.ru>
2021-11-24 15:21:08 +0300

    Post CRUD logic decomposed

--- PostController.js
+++ PostController.js
@@ -1,50 +1,46 @@
 import Post from './Post.js';
+import PostService from './PostService.js';
 
 class PostController {
   async create(request, response) {
     try {
-      const { author, title, content, picture } = request.body;
-      const post = await Post.create({ author, title, content, picture });
+      const post = await PostService.create(request.body);
       response.status(200).json(post);
     } catch (e) {
       response.status(500).json(e);
     }
   }
+
   async getAll(request, response) {
     try {
-      const posts = await Post.find();
+      const posts = await PostService.getAll();
       return response.json(posts);
     } catch (e) {
       response.status(500).json(e);
     }
   }
+
   async getOne(request, response) {
     try {
-      const { id } = request.params;
-      if (!id) response.status(400).json({ message: 'Id is missing' });
-      const post = await Post.findById(id);
+      const post = await PostService.getOne(request.params.id);
       return response.json(post);
     } catch (e) {
       response.status(500).json(e);
     }
   }
+
   async update(request, response) {
     try {
-      const post = request.body;
-      if (!post._id) response.status(400).json({ message: 'Id is missing' });
-      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
-        new: true,
-      });
+      const updatedPost = await PostService.update(request.body);
       return response.json(updatedPost);
     } catch (e) {
-      response.status(500).json(e);
+      response.status(500).json(e.message);
     }
   }
+
   async delete(request, response) {
     try {
-      const { id } = request.params;
-      if (!id) response.status(400).json({ message: 'Id is missing' });
-      const post = await Post.findByIdAndDelete(id);
+      const post = await PostService.delete(request.params.id);
       return response.json(post);
     } catch (e) {
       response.status(500).json(e);
